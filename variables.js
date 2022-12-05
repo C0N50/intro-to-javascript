ceecb224248e40e3350c2610de8a9f8a5565b7a2
Connor <115666141+C0N50@users.noreply.github.com>
2022-12-05 17:10:24 -0600

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,10 @@
+console.log('js');
+
+// declare and initialize a variable
+let testVar = 9;
+console.log(testVar);
+console.log( 'the value of testVar is', testVar);
+testVar = 55;
+console.log( 'the value of testVar is', testVar);
+testVar = 'some text';
+console.log( 'the value of testVar is', testVar);
